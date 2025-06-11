b4807e5487bd929094cfb4286f1b94233a37262f
bmahmoud21 <bam212@pitt.edu>
2025-06-11 10:43:08 -0400

    commits

--- pokemonprokec/src/App.jsx
+++ pokemonprokec/src/App.jsx
@@ -22,7 +22,12 @@ import gigaband from './imgs/gigaband.webp'
 import waterstone from './imgs/waterstone.webp'
 import wartortle from './imgs/wartotlegif.gif'
 import wartortlepokedex from './imgs/wartortlepokedex.webp'
-
+import Blast from './imgs/Blast.gif'
+import blastpokedex from './imgs/blastpokedex.png'
+import sable from './imgs/sableye.png'
+import sablepokedex from './imgs/sablepokedex.png'
+import oshowat from './imgs/oshowat.gif'
+import oshowatpokedex from './imgs/oshowatpokedex.webp'
 
 function App() {
     const delay = (ms) => new Promise((res) => setTimeout(res, ms));
@@ -77,7 +82,33 @@ function App() {
         setWartortle(showWartortle);
         setWartortlePokedex(false);
     }
- 
+
+    const [showBlast, setBlast] = React.useState(false);
+    const [showBlastPoof, setBlastPoof] = React.useState(false);
+    const [showBlastPokex, setBlastPokedex] = React.useState(true);
+    const [showWaterStoneBlast, setWaterStoneBlast] = React.useState(false);
+    const toggleWaterStoneBlast = () => {
+        setWaterStoneBlast(!showWaterStoneBlast);
+    }
+    const toggleBlast = async () => {
+        setBlastPoof(!showBlastPoof);
+        setIsVisible(!isVisible);
+        await delay(1000);
+        setTimeout(() => {
+            setBlastPoof(showBlastPoof);
+            setIsVisible(isVisible);
+        }, 1000);
+        await delay(1000);
+        setBlast(!showBlast);
+        BlastSetShowButon(!showBlastButton);
+
+    }
+    const [showBlastButton, BlastSetShowButon] = React.useState(false);
+    const handleClickBlastCard = () => {
+        BlastSetShowButon(!showBlastButton);
+        setBlast(showBlast);
+        setBlastPokedex(false);
+    }
 
 
 
@@ -100,6 +131,48 @@ function App() {
     const handleClickMimiCard = () => {
         mimiSetCard(!showMimiButton);
         setMimiPokedex(false);
+    }
+
+    const [showSablePokex, setSablePokedex] = React.useState(true);
+    const [sableShowButton, sableSetShowbutton] = React.useState(true);
+    const [showSableButton, sableSetCard] = React.useState(false);
+    const [showSablePoof, setSablePoof] = React.useState(false);
+    const sableToggleButton = async () => {
+        setSablePoof(!showSablePoof);
+        setIsVisible(!isVisible);
+        await delay(1000);
+        sableSetShowbutton(!sableShowButton);
+        setTimeout(() => {
+            setSablePoof(showSablePoof);
+            setIsVisible(isVisible);
+        }, 1000);
+        await delay(1000);
+        sableSetCard(!showSableButton);
+    }
+    const handleClickSableCard = () => {
+        sableSetCard(!showSableButton);
+        setSablePokedex(false);
+    }
+
+    const [showOshoPokex, setOshoPokedex] = React.useState(true);
+    const [oshoShowButton, oshoSetShowbutton] = React.useState(true);
+    const [showOshoButton, oshoSetCard] = React.useState(false);
+    const [showOshoPoof, setOshoPoof] = React.useState(false);
+    const oshoToggleButton = async () => {
+        setOshoPoof(!showOshoPoof);
+        setIsVisible(!isVisible);
+        await delay(1000);
+        oshoSetShowbutton(!oshoShowButton);
+        setTimeout(() => {
+            setOshoPoof(showOshoPoof);
+            setIsVisible(isVisible);
+        }, 1000);
+        await delay(1000);
+        oshoSetCard(!showOshoButton);
+    }
+    const handleClickOshoCard = () => {
+        oshoSetCard(!showOshoButton);
+        setOshoPokedex(false);
     }
 
 
@@ -216,6 +289,33 @@ function App() {
         )
     }
 
+    function SableCard() {
+        return (
+            <div className="cards">
+                <PokemonCard pokemon={{
+                    image: './src/Components/sableeyecard.gif',
+                    name: "Sableye",
+                    summary: "Sableye is often depicted as a creature that is feared for its glowing, sinister eyes, which are said to steal people's souls. However, it's also portrayed as having a playful and mischievous side, often pulling pranks on travelers to gain their attention. This behavior is suggested to stem from loneliness, as Sableye prefer to stay in dark caves and may crave interaction."
+                }}>
+                </PokemonCard>
+                <div className="unclickable"></div>
+            </div>
+        )
+    }
+
+    function OshoCard() {
+        return (
+            <div className="cards">
+                <PokemonCard pokemon={{
+                    image: './src/Components/osho.gif',
+                    name: "Oshawott",
+                    summary: "Oshawott, as a Pokemon, is known for being playful, curious, and occasionally mischievous. He is often shown as an attention-seeking and sometimes interfering Pokemon, similar to some of Ash's other past Pokemon. Oshawott can display a wide range of emotions, including happiness, sorrow, and even a hint of selfishness."
+                }}>
+                </PokemonCard>
+                <div className="unclickable"></div>
+            </div>
+        )
+    }
     function MunchCard() {
         return (
             <div className="cards">
@@ -249,8 +349,22 @@ function App() {
             <div className="cards">
                 <PokemonCard pokemon={{
                     image: './src/Components/gigsnorlaxcard.gif',
-                    name: "Gigantamax Snorlax",
+                    name: "Giga Snorlax",
                     summary: "Gigantamax Snorlax maintains Snorlax's signature laid-back, almost lethargic personality, but with a massive, almost mountain-sized scale. It's known for being incredibly lazy and only moves enough to eat and battle. Even in battle, it primarily relies on powerful, though slow, attacks, rather than agility."
+                }}>
+                </PokemonCard>
+                <div className="unclickable"></div>
+            </div>
+        )
+    }
+
+    function BlastCard() {
+        return (
+            <div className="cards">
+                <PokemonCard pokemon={{
+                    image: './src/Components/blastcard.gif',
+                    name: "Blastoid",
+                    summary: "Blastoise is portrayed as a fierce, powerful, and sometimes aggressive Pokemon, often depicted with a strong, determined demeanor. While not always explicitly described as mean, its aggressive tendencies are apparent in its combat style, particularly when using its powerful water jets for high-speed tackles.",
                 }}>
                 </PokemonCard>
                 <div className="unclickable"></div>
@@ -302,6 +416,25 @@ function App() {
                         {showWartortleButton && <button className="cardButton" onClick={handleClickWartortleCard}><div class="btn-text">Catch me!</div></button>}
                     </div>
 
+                    <div className="Blastoid">
+                        {showWartortle && !showWaterStoneBlast &&
+                            <button onClick={toggleBlast, toggleWaterStoneBlast}>
+                                <img className="waterstoneblast" src={waterstone} ></img>
+                            </button>
+                        }
+
+                        {showWaterStone && showWartortle && showWaterStoneBlast && !showBlast &&
+                            <button onClick={toggleBlast}>
+                                <img className="Blastimg" src={Blast}></img>
+                            </button>}
+                        {showBlastPoof && <img className="Blastpoof" src={poof}></img>}
+                        {isVisible &&
+                            <div className="unclickablepoof"></div>
+                        }
+                        {showBlast && showBlastButton && <BlastCard></BlastCard>}
+                        {showBlastButton && <button className="cardButton" onClick={handleClickBlastCard}><div class="btn-text">Catch me!</div></button>}
+                    </div>
+
                     <div className="mimi">
                         {mimiShowButton && <button className="button" onClick={mimiToggleButton}>
                             <img className="mimiimg" src={mimikyuu}></img>
@@ -312,6 +445,30 @@ function App() {
                         }
                         {showMimiButton && <MimikyuuCard />}
                         {showMimiButton && <button className="cardButton" onClick={handleClickMimiCard}><div class="btn-text">Catch me!</div></button>}
+                    </div>
+
+                    <div className="sable">
+                        {sableShowButton && <button className="button" onClick={sableToggleButton}>
+                            <img className="sableimg" src={sable}></img>
+                        </button>}
+                        {showSablePoof && <img className="sablepoof" src={poof}></img>}
+                        {isVisible &&
+                            <div className="unclickablepoof"></div>
+                        }
+                        {showSableButton && <SableCard />}
+                        {showSableButton && <button className="cardButton" onClick={handleClickSableCard}><div class="btn-text">Catch me!</div></button>}
+                    </div>
+
+                    <div className="osho">
+                        {oshoShowButton && <button className="button" onClick={oshoToggleButton}>
+                            <img className="oshoimg" src={oshowat}></img>
+                        </button>}
+                        {showOshoPoof && <img className="oshopoof" src={poof}></img>}
+                        {isVisible &&
+                            <div className="unclickablepoof"></div>
+                        }
+                        {showOshoButton && <OshoCard />}
+                        {showOshoButton && <button className="cardButton" onClick={handleClickOshoCard}><div class="btn-text">Catch me!</div></button>}
                     </div>
 
                     <div className="munch">
@@ -395,6 +552,19 @@ function App() {
                         <div className="wartortlehint">Find the WaterStone to evolve Squirtle and unlock me!</div>
                     }
                     {!showWartortlePokex && <img src={wartortlepokedex} className="wartortlepokedexActive"></img>}
+                    {showBlastPokex && <img src={blastpokedex} className="blastpokedex"></img>}
+                    {!showWaterStoneBlast && !showWartortle &&
+                        <div className="blasthint"></div>
+                    }
+                    {!showWaterStoneBlast && !showBlast && !showWartortlePokex &&
+                        <div className="blasthint">Find the WaterStone to evolve Wartortle and unlock me!</div>
+                    }
+                    {!showBlastPokex && <img src={blastpokedex} className="blastpokedexActive"></img>}
+                    {showSablePokex && <img src={sablepokedex} className="sablepokedex"></img>}
+                    {!showSablePokex && <img src={sablepokedex} className="sablepokedexActive"></img>}
+
+                    {showOshoPokex && <img src={oshowatpokedex} className="oshopokedex"></img>}
+                    {!showOshoPokex && <img src={oshowatpokedex} className="oshopokedexActive"></img>}
 
                 </div>
             </div>
--- pokemonprokec/src/Components/PokemonCard.jsx
+++ pokemonprokec/src/Components/PokemonCard.jsx
@@ -6,7 +6,9 @@ function PokemonCard({ pokemon }) {
             </img>
             <div className="pokemon-info">
                 <h3>{pokemon.name}</h3>
-                <p>{pokemon.summary}</p>
+                <div className="sum">
+                    <p>{pokemon.summary}</p>
+                </div>
             </div>
         </div>
     );
